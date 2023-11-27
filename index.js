ac5e7532b6dd24da4ba08f8dd86492efd2057ecd
Tansimrinky <tansimrinkybindu@gmail.com>
2023-11-28 00:53:54 +0600

    course count added

--- index.js
+++ index.js
@@ -8,6 +8,70 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fsmcn5d.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+   
+    
+
+
+  try {
+
+
+    const courseCollection = client.db("SkillBoostDB").collection("courses")
+    const userCollection = client.db("SkillBoostDB").collection("user")
+
+  
+    app.get('/courses' , async(req, res) => {
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size)
+        console.log('pagination', page, size)
+        const result =await courseCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray()
+        res.send(result)
+    })
+    
+
+    app.get('/coursesCount', async(req, res) => {
+      const count = await courseCollection.estimatedDocumentCount();
+      res.send({count})
+    })
+
+    app.post('/users', async(req, res) => {
+      const user = req.body
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('SkillBoost HUb is running')
 })
