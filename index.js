e83ed232bdc10741cd64700303b9ae4c7fa9dad2
Tansimrinky <tansimrinkybindu@gmail.com>
2023-11-28 12:57:41 +0600

    single course api added

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fsmcn5d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,48 +17,56 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
-   
-    
-
-
   try {
+    const courseCollection = client.db("SkillBoostDB").collection("courses");
+    const userCollection = client.db("SkillBoostDB").collection("user");
 
-
-    const courseCollection = client.db("SkillBoostDB").collection("courses")
-    const userCollection = client.db("SkillBoostDB").collection("user")
-
-  
-    app.get('/courses' , async(req, res) => {
-        const page = parseInt(req.query.page);
-        const size = parseInt(req.query.size)
-        console.log('pagination', page, size)
-        const result =await courseCollection.find()
+    app.get("/courses", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log("pagination", page, size);
+      const result = await courseCollection
+        .find()
         .skip(page * size)
         .limit(size)
-        .toArray()
-        res.send(result)
-    })
-    
+        .toArray();
+      res.send(result);
+    });
 
-    app.get('/coursesCount', async(req, res) => {
+    app.get("/courses/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await courseCollection.findOne(query);
+      res.send(result
+        )
+    })
+
+    app.get("/coursesCount", async (req, res) => {
       const count = await courseCollection.estimatedDocumentCount();
-      res.send({count})
-    })
+      res.send({ count });
+    });
 
-    app.post('/users', async(req, res) => {
-      const user = req.body
-      const result = await userCollection.insertOne(user)
-      res.send(result)
-    })
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -67,15 +74,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('SkillBoost HUb is running')
-})
+app.get("/", (req, res) => {
+  res.send("SkillBoost HUb is running");
+});
 
 app.listen(port, () => {
-    console.log(`SkillBoost Hub is running on the port ${port}`)
-})
+  console.log(`SkillBoost Hub is running on the port ${port}`);
+});
